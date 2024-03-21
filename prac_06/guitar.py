3554c96d86ad42ed731455a66e56b5d7462f409e
Wilson Vallerian <wilsonvallerianwong@gmail.com>
2024-03-21 12:23:12 +0800

    Add docstring to current file.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,18 +3,22 @@ VINTAGE_YEAR = 50
 
 
 class Guitar:
-
+    """Represent a guitar object."""
     def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a guitar object."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Defines the string representation."""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def get_age(self):
+        """Determine guitar age."""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
+        """Determine if guitar is vintage."""
         return self.get_age() >= VINTAGE_YEAR
 
